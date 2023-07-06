7248371f15172ca621a083b2baf24738144c9943
Ana Paula Lesniovski <anap.lesniovski@gmail.com>
2023-07-05 21:24:24 -0300

    Adicionado estrutura de dados e endpoint integrado.

--- MeatlessApp/Service/Service.swift
+++ MeatlessApp/Service/Service.swift
@@ -3,8 +3,16 @@ import Foundation
 struct Service {
     
     func fetchRestaurantList(completion: @escaping ([Restaurant]?)->Void) {
-        
-        // STORY 1: Implement the API integration to fetch restaurant list.
-         
+        guard let baseUrl = URL(string: "https://raw.githubusercontent.com/devpass-tech/meatless-api/main/restaurant_list.json") else { return }
+        let task = URLSession.shared.dataTask(with: baseUrl) { (data, response, error) in
+            guard let responseData = data else { return }
+            do {
+                let restauratList = try JSONDecoder().decode([Restaurant].self, from: responseData)
+                completion(restauratList)
+            } catch let error {
+                print("Error: \(error)")
+            }
+        }
+        task.resume()
     }
 }
